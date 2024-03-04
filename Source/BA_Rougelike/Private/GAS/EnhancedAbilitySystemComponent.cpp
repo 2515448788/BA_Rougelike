3da201b7979af06cb4c9c2c7658609f5dff0759d
Luna_K <65744111+2515448788@users.noreply.github.com>
2024-03-04 11:32:31 +0800

    ASC组件的注释完善

--- Source/BA_Rougelike/Private/GAS/EnhancedAbilitySystemComponent.cpp
+++ Source/BA_Rougelike/Private/GAS/EnhancedAbilitySystemComponent.cpp
@@ -5,8 +5,19 @@
 #include "InputAction.h"
 #include "EnhancedInputComponent.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemGlobals.h"
 
+
+/*
+ *************注释变量对照表 *************
+ *
+ * MappedAbilities -> 能力绑定映射表
+ * FAbilityInputBinding -> 能力绑定信息（结构体）
+ * BoundAbilitiesStack -> 能力句柄列表
+ * 
+ */
+
+
+//命名空间 用于获取下一个有效的输入ID
 namespace EnhancedInputAbilitySystem_Impl
 {
 	constexpr int32 InvalidInputID = 0;
@@ -19,186 +30,98 @@ namespace EnhancedInputAbilitySystem_Impl
 }
 
 
-/**
- * 设置输入绑定
- * @param InputAction 输入动作
- * @param AbilityHandle 能力句柄
- */
-void UEnhancedInputAbilitySystem::SetInputBinding(UInputAction* InputAction, FGameplayAbilitySpecHandle AbilityHandle)
+//覆写BeginPlay函数 主要是获取增强输入组件
+void UEnhancedInputAbilitySystem::BeginPlay()
 {
-	using namespace EnhancedInputAbilitySystem_Impl;
-	// 查找能力句柄对应的FGameplayAbilitySpec对象
-	FGameplayAbilitySpec* BindingAbility = FindAbilitySpec(AbilityHandle);
-	// 查找输入动作对应的FAbilityInputBinding对象
-	FAbilityInputBinding* AbilityInputBinding = MappedAbilities.Find(InputAction);
-	// 如果存在对应的FAbilityInputBinding对象
-	if (AbilityInputBinding)
+	Super::BeginPlay(); // 调用父类的BeginPlay函数
+	AActor* Owner = GetOwner(); // 获取Owner
+	if (IsValid(Owner) && Owner->InputComponent) // 如果Owner存在且有InputComponent
 	{
-		// 获取当前绑定的FGameplayAbilitySpec对象
-		FGameplayAbilitySpec* OldBoundAbility = FindAbilitySpec(AbilityInputBinding->BoundAbilitiesStack.Top());
-		// 如果当前绑定的FGameplayAbilitySpec对象与输入动作对应的FGameplayAbilitySpec对象相同
-		if (OldBoundAbility && OldBoundAbility->InputID == AbilityInputBinding->InputID)
-		{
-			// 将当前绑定的FGameplayAbilitySpec对象的InputID设置为无效值
-			OldBoundAbility->InputID = InvalidInputID;
-		}
+		// 将Owner的输入组件尝试转换为增强输入组件
+		InputComponent = CastChecked<UEnhancedInputComponent>(Owner->InputComponent);
 	}
-	else
-	{
-		// 如果不存在对应的FAbilityInputBinding对象，则创建一个新的FAbilityInputBinding对象
-		AbilityInputBinding = &MappedAbilities.Add(InputAction);
-		// 设置新的FAbilityInputBinding对象的InputID为下一个可用的InputID
-		AbilityInputBinding->InputID = GetNextInputID();
-	}
-	// 如果存在对应的FGameplayAbilitySpec对象
-	if (BindingAbility)
-	{
-		// 将FGameplayAbilitySpec对象的InputID设置为对应的FAbilityInputBinding对象的InputID
-		BindingAbility->InputID = AbilityInputBinding->InputID;
-	}
-	// 将能力句柄添加到FAbilityInputBinding对象的BoundAbilitiesStack中
-	AbilityInputBinding->BoundAbilitiesStack.Push(AbilityHandle);
-	// 尝试绑定输入动作和对应的FAbilityInputBinding对象
-	TryBindAbilityInput(InputAction, *AbilityInputBinding);
 }
 
+/**
+ * 根据能力的句柄查找能力
+ *
+ * @param Handle 能力句柄
+ * @return 找到的能力指针
+ */
+FGameplayAbilitySpec* UEnhancedInputAbilitySystem::FindAbilitySpec(FGameplayAbilitySpecHandle Handle)
+{
+	// 根据句柄查找能力实例
+	FGameplayAbilitySpec* FoundAbility = nullptr;
+	// 父类函数
+	FoundAbility = FindAbilitySpecFromHandle(Handle);
+	return FoundAbility;
+}
 
 /**
- * 清除输入绑定
- * @param AbilityHandle 能力句柄
+ * 清除指定输入操作绑定的所有能力
+ * @param InputAction 要移除的输入动作
  */
-void UEnhancedInputAbilitySystem::ClearInputBinding(FGameplayAbilitySpecHandle AbilityHandle)
+void UEnhancedInputAbilitySystem::RemoveEntry(UInputAction* InputAction)
 {
-	using namespace EnhancedInputAbilitySystem_Impl;
-	if (FGameplayAbilitySpec* FoundAbility = FindAbilitySpec(AbilityHandle))
+	// 从能力绑定映射表中查找传入输入操作对应的能力绑定信息
+	if (FAbilityInputBinding* AbilityInputBinding = MappedAbilities.Find(InputAction))
 	{
-		auto MappedIterator = MappedAbilities.CreateIterator();
-		while (MappedIterator)
+		// 若增强输入组件有效
+		if (InputComponent)
 		{
-			if (MappedIterator.Value().InputID == FoundAbility->InputID)
-			{
-				break;
-			}
-			++MappedIterator;
+			// 移除按下的绑定
+			InputComponent->RemoveBindingByHandle(AbilityInputBinding->OnPressedHandle);
+			// 移除释放的绑定
+			InputComponent->RemoveBindingByHandle(AbilityInputBinding->OnReleasedHandle);
 		}
-
-		if (MappedIterator)
+		// 遍历能力句柄列表
+		for (FGameplayAbilitySpecHandle AbilityHandle : AbilityInputBinding->BoundAbilitiesStack)
 		{
-			FAbilityInputBinding& AbilityInputBinding = MappedIterator.Value();
-			if (AbilityInputBinding.BoundAbilitiesStack.Remove(AbilityHandle) > 0)
+			using namespace EnhancedInputAbilitySystem_Impl;
+			// 获取能力实例
+			FGameplayAbilitySpec* AbilitySpec = FindAbilitySpec(AbilityHandle);
+			// 若能力实例存在且其InputID与能力绑定信息的InputID相同
+			if (AbilitySpec && AbilitySpec->InputID == AbilityInputBinding->InputID)
 			{
-				if (AbilityInputBinding.BoundAbilitiesStack.Num() > 0)
-				{
-					FGameplayAbilitySpec* StackedAbility = FindAbilitySpec(
-						AbilityInputBinding.BoundAbilitiesStack.Top());
-					if (StackedAbility && StackedAbility->InputID == 0)
-					{
-						StackedAbility->InputID = AbilityInputBinding.InputID;
-					}
-				}
-				else
-				{
-					RemoveEntry(MappedIterator.Key());
-				}
-				FoundAbility->InputID = InvalidInputID;
+				// 将能力实例InputID设置为0
+				AbilitySpec->InputID = InvalidInputID;
 			}
 		}
+		// 从能力绑定映射表中移除对应输入操作的部分
+		MappedAbilities.Remove(InputAction);
 	}
 }
 
-
 /**
- * 清除指定输入动作的绑定
- * @param InputAction 要清除绑定的输入动作
- */
-void UEnhancedInputAbilitySystem::ClearAbilityBindings(UInputAction* InputAction)
-{
-	RemoveEntry(InputAction); // 从系统中移除指定的输入动作
-}
-
-
-/**
- * 当能力输入按下时的回调函数
+ * 回调函数 输入操作按下时调用
  * @param InputAction 输入动作
  */
 void UEnhancedInputAbilitySystem::OnAbilityInputPressed(UInputAction* InputAction)
 {
 	using namespace EnhancedInputAbilitySystem_Impl;
-	// 查找与输入动作对应的绑定
-	FAbilityInputBinding* FoundBinding = MappedAbilities.Find(InputAction);
-	if (FoundBinding && ensure(FoundBinding->InputID != InvalidInputID))
+	// 根据输入操作从能力绑定映射表中查找对应的能力绑定信息
+	FAbilityInputBinding* AbilityInputBinding = MappedAbilities.Find(InputAction);
+	if (AbilityInputBinding && ensure(AbilityInputBinding->InputID != InvalidInputID))
 	{
-		// 调用父类方法
-		AbilityLocalInputPressed(FoundBinding->InputID);
+		// 核心-若传入的能力绑定信息存在且其InputID不等于0 那么调用父类的AbilityLocalInputPressed方法
+		AbilityLocalInputPressed(AbilityInputBinding->InputID);
 	}
 }
 
-
-// 尝试通过输入操作调用父类的AbilityLocalInputReleased方法
+/**
+ * 回调函数 输入操作松开时调用
+ * @param InputAction 输入动作
+ */
 void UEnhancedInputAbilitySystem::OnAbilityInputReleased(UInputAction* InputAction)
 {
 	using namespace EnhancedInputAbilitySystem_Impl;
-	// 查找与输入操作对应的AbilityInputBinding
-	FAbilityInputBinding* FoundBinding = MappedAbilities.Find(InputAction);
-	if (FoundBinding && ensure(FoundBinding->InputID != InvalidInputID))
+	// 根据输入操作从能力绑定映射表中查找对应的能力绑定信息
+	FAbilityInputBinding* AbilityInputBinding = MappedAbilities.Find(InputAction);
+	if (AbilityInputBinding && ensure(AbilityInputBinding->InputID != InvalidInputID))
 	{
-		// 调用父类的AbilityLocalInputReleased方法
-		AbilityLocalInputReleased(FoundBinding->InputID);
+		// 核心-若传入的能力绑定信息存在且其InputID不等于0 那么调用父类的AbilityLocalInputReleased方法
+		AbilityLocalInputReleased(AbilityInputBinding->InputID);
 	}
-}
-
-
-/**
- * @brief 从映射的技能列表中移除指定的输入动作
- * 
- * @param InputAction 要移除的输入动作
- */
-void UEnhancedInputAbilitySystem::RemoveEntry(UInputAction* InputAction)
-{
-	// 查找输入动作对应的绑定
-	if (FAbilityInputBinding* Bindings = MappedAbilities.Find(InputAction))
-	{
-		// 如果存在输入组件
-		if (InputComponent)
-		{
-			// 移除按下的绑定
-			InputComponent->RemoveBindingByHandle(Bindings->OnPressedHandle);
-			// 移除释放的绑定
-			InputComponent->RemoveBindingByHandle(Bindings->OnReleasedHandle);
-		}
-		// 遍历绑定的技能栈
-		for (FGameplayAbilitySpecHandle AbilityHandle : Bindings->BoundAbilitiesStack)
-		{
-			// 使用增强输入能力系统插件的命名空间
-			using namespace EnhancedInputAbilitySystem_Impl;
-
-			// 查找对应的技能规范
-			FGameplayAbilitySpec* AbilitySpec = FindAbilitySpec(AbilityHandle);
-			// 如果找到对应的技能规范且输入ID与绑定的输入ID相同
-			if (AbilitySpec && AbilitySpec->InputID == Bindings->InputID)
-			{
-				// 将输入ID设置为无效值
-				AbilitySpec->InputID = InvalidInputID;
-			}
-		}
-		// 从映射的技能列表中移除输入动作
-		MappedAbilities.Remove(InputAction);
-	}
-}
-
-
-/**
- * 根据能力规格的句柄查找能力规格
- *
- * @param Handle 能力规格的句柄
- * @return 找到的能力规格指针
- */
-FGameplayAbilitySpec* UEnhancedInputAbilitySystem::FindAbilitySpec(FGameplayAbilitySpecHandle Handle)
-{
-	// 从句柄中查找能力规格
-	FGameplayAbilitySpec* FoundAbility = nullptr;
-	FoundAbility = FindAbilitySpecFromHandle(Handle);
-	return FoundAbility;
 }
 
 /**
@@ -214,6 +137,7 @@ void UEnhancedInputAbilitySystem::TryBindAbilityInput(UInputAction* InputAction,
 		// 按下事件
 		if (AbilityInputBinding.OnPressedHandle == 0)
 		{
+			//***核心操作 将OnAbilityInputPressed回调函数绑定到输入操作的按下事件***//
 			AbilityInputBinding.OnPressedHandle = InputComponent->BindAction(
 				InputAction, ETriggerEvent::Started, this, &UEnhancedInputAbilitySystem::OnAbilityInputPressed,
 				InputAction).GetHandle();
@@ -221,6 +145,7 @@ void UEnhancedInputAbilitySystem::TryBindAbilityInput(UInputAction* InputAction,
 		// 释放事件
 		if (AbilityInputBinding.OnReleasedHandle == 0)
 		{
+			//***核心操作 将OnAbilityInputReleased回调函数绑定到输入操作的释放事件***//
 			AbilityInputBinding.OnReleasedHandle = InputComponent->BindAction(
 				InputAction, ETriggerEvent::Completed, this, &UEnhancedInputAbilitySystem::OnAbilityInputReleased,
 				InputAction).GetHandle();
@@ -229,15 +154,106 @@ void UEnhancedInputAbilitySystem::TryBindAbilityInput(UInputAction* InputAction,
 }
 
 /**
- * @brief UEnhancedInputAbilitySystem::BeginPlay函数的实现
+ * 设置输入绑定
+ * @param InputAction 输入动作
+ * @param AbilityHandle 能力句柄
  */
-void UEnhancedInputAbilitySystem::BeginPlay()
+void UEnhancedInputAbilitySystem::SetInputBinding(UInputAction* InputAction, FGameplayAbilitySpecHandle AbilityHandle)
 {
-	Super::BeginPlay(); // 调用父类的BeginPlay函数
-	AActor* Owner = GetOwner(); // 获取Owner
-	if (IsValid(Owner) && Owner->InputComponent) // 如果Owner存在且有InputComponent
+	using namespace EnhancedInputAbilitySystem_Impl;
+	// 通过输入动作查找对应的能力绑定信息
+	FAbilityInputBinding* AbilityInputBinding = MappedAbilities.Find(InputAction);
+	//当前输入操作存在有效的能力绑定信息
+	if (AbilityInputBinding)
 	{
-		// 将Owner的InputComponent转换为UEnhancedInputComponent类型并赋值给InputComponent
-		InputComponent = CastChecked<UEnhancedInputComponent>(Owner->InputComponent);
+		//根据找到的能力绑定信息，获取能力句柄列表的最后一项，并查找其对应的能力实例 （Top函数返回TArray的末尾元素）
+		FGameplayAbilitySpec* OldBoundAbility = FindAbilitySpec(AbilityInputBinding->BoundAbilitiesStack.Top());
+		if (OldBoundAbility && OldBoundAbility->InputID == AbilityInputBinding->InputID)
+		{
+			//若能力实例存在且其InputID和当前能力绑定信息的InputID相同 那么旧的能力实例的InputID设置为0
+			OldBoundAbility->InputID = InvalidInputID;
+		}
 	}
+	else
+	{
+		// 当前输入操作无能力绑定信息，则创建一个新的能力绑定信息
+		AbilityInputBinding = &MappedAbilities.Add(InputAction);
+		// 设置新的能力绑定信息的InputID为下一个可用的InputID
+		AbilityInputBinding->InputID = GetNextInputID();
+	}
+	// 通过能力句柄查找对应的能力实例
+	FGameplayAbilitySpec* BindingAbility = FindAbilitySpec(AbilityHandle);
+	if (BindingAbility)
+	{
+		// 将能力实例的InputID设置为对应的能力绑定信息的InputID
+		BindingAbility->InputID = AbilityInputBinding->InputID;
+	}
+	// 将能力句柄添加到能力绑定信息的能力句柄列表中（Push向TArray末尾添加元素）
+	AbilityInputBinding->BoundAbilitiesStack.Push(AbilityHandle);
+	// 尝试绑定输入动作和对应的能力绑定信息
+	TryBindAbilityInput(InputAction, *AbilityInputBinding);
+}
+
+/**
+ * 清除指定能力绑定的输入操作
+ * @param AbilityHandle 能力句柄
+ */
+void UEnhancedInputAbilitySystem::ClearInputBinding(FGameplayAbilitySpecHandle AbilityHandle)
+{
+	using namespace EnhancedInputAbilitySystem_Impl;
+	//查找传入能力句柄对应的能力实例
+	FGameplayAbilitySpec* FoundAbility = FindAbilitySpec(AbilityHandle);
+	if (FoundAbility)
+	{
+		//若传入的能力句柄存在对应的能力实例 那么为 能力绑定映射表 创建迭代器
+		auto MappedIterator = MappedAbilities.CreateIterator();
+		//遍历
+		while (MappedIterator)
+		{
+			//找到相同的InputID
+			if (MappedIterator.Value().InputID == FoundAbility->InputID)
+			{
+				break;
+			}
+			//迭代器自增
+			++MappedIterator;
+		}
+		//若迭代器仍有效（表示找到了InputID相同的能力绑定）
+		if (MappedIterator)
+		{
+			//获取找到的能力绑定信息
+			FAbilityInputBinding& AbilityInputBinding = MappedIterator.Value();
+			//若移除的能力句柄数量大于0
+			if (AbilityInputBinding.BoundAbilitiesStack.Remove(AbilityHandle) > 0)
+			{
+				//剩下的能力句柄列表数量大于0
+				if (AbilityInputBinding.BoundAbilitiesStack.Num() > 0)
+				{
+					//获取最后绑定的能力并尝试将其 InputID 设置为 能力绑定信息 的 InputID
+					FGameplayAbilitySpec* StackedAbility = FindAbilitySpec(
+						AbilityInputBinding.BoundAbilitiesStack.Top());
+					if (StackedAbility && StackedAbility->InputID == 0)
+					{
+						StackedAbility->InputID = AbilityInputBinding.InputID;
+					}
+				}
+				else
+				{
+					//该 能力绑定信息 已经没有绑定的能力句柄列表 移除绑定入口
+					RemoveEntry(MappedIterator.Key());
+				}
+				//将传入的能力的InputID重置为0
+				FoundAbility->InputID = InvalidInputID;
+			}
+		}
+	}
+}
+
+/**
+ * 清除指定输入操作绑定的所有能力
+ * @param InputAction 输入操作
+ */
+void UEnhancedInputAbilitySystem::ClearAbilityBindings(UInputAction* InputAction)
+{
+	RemoveEntry(InputAction);
 }
--- Source/BA_Rougelike/Public/GAS/EnhancedAbilitySystemComponent.h
+++ Source/BA_Rougelike/Public/GAS/EnhancedAbilitySystemComponent.h
@@ -5,18 +5,28 @@
 #include "EnhancedInputComponent.h" // for FInputBindingHandle
 #include "EnhancedAbilitySystemComponent.generated.h"
 
-
 class UInputAction;
 
+
+/*
+ *************注释变量对照表 *************
+ *
+ * MappedAbilities -> 能力绑定映射表
+ * FAbilityInputBinding -> 能力绑定信息（结构体）
+ * BoundAbilitiesStack -> 能力句柄列表
+ * 
+ */
+
+
+//自定义结构体 储存能力绑定相关信息
 USTRUCT()
 struct FAbilityInputBinding
 {
 	GENERATED_BODY()
-
-	int32 InputID = 0;
-	uint32 OnPressedHandle = 0;
-	uint32 OnReleasedHandle = 0;
-	TArray<FGameplayAbilitySpecHandle> BoundAbilitiesStack;
+	int32 InputID = 0; //输入ID
+	uint32 OnPressedHandle = 0; //按下操作句柄
+	uint32 OnReleasedHandle = 0; //释放操作句柄
+	TArray<FGameplayAbilitySpecHandle> BoundAbilitiesStack; //已绑定的能力句柄列表
 };
 
 /**
@@ -28,32 +38,34 @@ class BA_ROUGELIKE_API UEnhancedInputAbilitySystem : public UAbilitySystemCompon
 	GENERATED_BODY()
 
 public:
-	//绑定能力输入-单个
+	//绑定能力输入
 	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
 	void SetInputBinding(UInputAction* InputAction, FGameplayAbilitySpecHandle AbilityHandle);
-	//清除能力输入-单个 
+	//清除指定能力绑定的输入操作
 	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
 	void ClearInputBinding(FGameplayAbilitySpecHandle AbilityHandle);
-	///清除能力输入-所有
+	///清除指定输入操作绑定的所有能力
 	UFUNCTION(BlueprintCallable, Category = "Enhanced Input Abilities")
 	void ClearAbilityBindings(UInputAction* InputAction);
 
 private:
+	virtual void BeginPlay() override;
+
+	FGameplayAbilitySpec* FindAbilitySpec(FGameplayAbilitySpecHandle Handle);
+
+	void RemoveEntry(UInputAction* InputAction);
+
 	void OnAbilityInputPressed(UInputAction* InputAction);
 
 	void OnAbilityInputReleased(UInputAction* InputAction);
 
-	void RemoveEntry(UInputAction* InputAction);
-
 	void TryBindAbilityInput(UInputAction* InputAction, FAbilityInputBinding& AbilityInputBinding);
 
-	FGameplayAbilitySpec* FindAbilitySpec(FGameplayAbilitySpecHandle Handle);
 
-	virtual void BeginPlay() override;
-
+	//能力绑定映射表
 	UPROPERTY(transient)
 	TMap<UInputAction*, FAbilityInputBinding> MappedAbilities;
-
+	//ASC组件拥有者的增强输入组件
 	UPROPERTY(transient)
 	UEnhancedInputComponent* InputComponent;
 };
