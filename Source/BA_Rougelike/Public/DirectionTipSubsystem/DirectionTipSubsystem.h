0f0b9ccff776b93e9cbc7cb21ceded8d3000cfbd
Luna_K <65744111+2515448788@users.noreply.github.com>
2024-05-09 13:13:54 +0800

    项目转5.4、方向提示子系统待完成、互动系统待完成、队友扶起功能待完成

---
+++ Source/BA_Rougelike/Public/DirectionTipSubsystem/DirectionTipSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "DirectionTipSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, Blueprintable)
+class BA_ROUGELIKE_API UDirectionTipSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
